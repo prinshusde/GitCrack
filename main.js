d10a9d5b1f2534dbe35435262e13988f31d8f076
Prince Shukla <prinshusdework@gmail.com>
2026-05-31 13:07:13 +0530

    second commit

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log("Hello World");
 const name ="prinshu"
+DOB = "1999-01-01"
---
+++ script.js
@@ -0,0 +1,13 @@
+// add function in javascript
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
+
+// subtract function in javascript
+function subtract(a, b) {
+    return a - b;
+}
+console.log(subtract(1, 2));
+
+// multiply function in javascript
